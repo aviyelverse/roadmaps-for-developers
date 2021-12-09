fe0c5a8abbfb073f29773c915db70ea3a1eb3be4
pramit-marattha <pramitmarattha5497@gmail.com>
2021-12-09 20:54:08 +0545

    MainHero color issue and font issue fixed

--- components/MainHero.js
+++ components/MainHero.js
@@ -4,7 +4,7 @@ const MainHero = () => {
   return (
     <section className="relative container flex flex-col-reverse lg:flex-row gap-12 mt-14 lg:mt-28 w-full">
       <div className="flex flex-1 flex-col items-center lg:items-start">
-        <h2 className="text-template-blue text-3xl md:text-4xl lg:text-5xl text-center lg:text-left mb-6">
+        <h2 className="text-template-blue text-3xl md:text-4xl lg:text-8xl text-center lg:text-left mb-6">
           Frontend Roadmap
         </h2>
         <p className="text-template-grey text-lg text-center lg:text-left mb-6">
@@ -19,7 +19,7 @@ const MainHero = () => {
           >
             <button
               type="button"
-              className="btn flex items-center bg-transparent hover:bg-green-500 text-green-700 font-semibold hover:text-white py-5 px-7 border border-green-500 hover:border-transparent rounded-lg"
+              className="btn flex items-center bg-transparent hover:bg-green-500 text-template-purple font-semibold hover:text-white py-5 px-7 border border-template-purple hover:border-transparent rounded-lg"
             >
               Full Roadmap on Github ➤
               <Image
@@ -39,7 +39,7 @@ const MainHero = () => {
           className="w-5/6 h-5/6 sm:w-3/4 sm:h-3/4 lg:w-full lg:h-full rounded-lg"
         />
       </div>
-      <div className="hidden lg:block overflow-hidden bg-green-500 rounded-lg py-3 px-6 bg-opacity-75 absolute h-80 w-1/2 top-32 right-0 lg:-bottom-28 lg:right-0"></div>
+      <div className="hidden lg:block overflow-hidden bg-template-purple rounded-lg py-3 px-6 bg-opacity-75 absolute h-80 w-1/2 top-32 right-0 lg:-bottom-28 lg:right-0"></div>
     </section>
   );
 };
